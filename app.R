f02d3964f91c211023994ed980309589dfe838f4
rgottberg <rgottberg@gmail.com>
2025-03-07 15:26:50 +0100

    replace missing values

--- app.R
+++ app.R
@@ -1,10 +1,12 @@
 # install libraries
 install.packages("ggplot2")
 install.packages("dplyr")
+install.packages("tidyr")
 
 #load libraries
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 
 #explore dataset
 data()
@@ -14,3 +16,20 @@ data(airquality)
 head(airquality)
 summary(airquality)
 str(airquality)
+
+
+#create dataframe
+df <- data.frame(airquality)
+
+#identify missing values
+colSums(is.na(airquality))
+
+# Replace NA values in multiple columns with zero
+df2 <- df |>
+    mutate(across(where(is.numeric), ~replace_na(., 0)))
+
+
+
+
+
+
