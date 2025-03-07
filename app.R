cb6264139099e33134fb45b4d4f99e3057bc7716
rgottberg <rgottberg@gmail.com>
2025-03-07 15:36:12 +0100

    create new column with date

--- app.R
+++ app.R
@@ -2,11 +2,13 @@
 install.packages("ggplot2")
 install.packages("dplyr")
 install.packages("tidyr")
+install.packages("lubridate")
 
 #load libraries
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(lubridate)
 
 #explore dataset
 data()
@@ -24,12 +26,14 @@ df <- data.frame(airquality)
 #identify missing values
 colSums(is.na(airquality))
 
-# Replace NA values in multiple columns with zero
+#replace NA values in multiple columns with zero
 df2 <- df |>
     mutate(across(where(is.numeric), ~replace_na(., 0)))
 
-
-
+#create new column
+df3 <- df2 |>
+    mutate(date = paste(Month,Day,"2025",sep='-')) |>
+    mutate(date = lubridate::mdy(date))
 
 
 
